a37185054ae865f55f93c22c1296e481a33b2fb9
David <41311088+biagiola@users.noreply.github.com>
2019-11-20 12:09:16 -0300

    Update 01 type annotation.ts

--- 01 type annotation.ts
+++ 01 type annotation.ts
@@ -1,10 +1,10 @@
 // static variable
-let person: string = "awais";
-let vectores  = [5, 2, 'John', true, false, 77.7 ]; // different types of data, similar a js, not avantage of typescript
+let person: string = "jonhanes";
+let vectores  = [5, 2, 'John', true, false, 77.7 ]; // different types of data, similar to js, not avantage of typescript but is valid
 let names: string[] = ['Louis', 'Davis', '4', 'Joe']; // array of strings
 let phones: Array<string> = ['apple', 'samsung']; // array of strings
-let height: number = 182.3546;
-let booleanValues: boolean[] = [true, false, false]; 
+let height: number = 182.3546; // number
+let booleanValues: boolean[] = [true, false, false]; // array of booleans
 
 // dynamic variable
 let variable: any = 5;
