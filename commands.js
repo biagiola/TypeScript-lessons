15b2f72f12f3deedf1f0e2ee8785d038663bf533
David <41311088+biagiola@users.noreply.github.com>
2019-11-23 10:29:03 -0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -2,3 +2,4 @@ npm install - g typescript; // to install
 tsc; // see all the commands.
 tsc app.ts; // in current directory transpile ts file into js.
 tsc - w apps.ts; // transpile in the watch mode.
+tsc apps.ts | node apps.js // runs the two commands at the same time
