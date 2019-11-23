ccfbe02eb6755bc5b81eaa692e253bb3bdccf532
David <41311088+biagiola@users.noreply.github.com>
2019-11-23 10:24:43 -0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,6 +1,4 @@
-npm;
-install - g;
-typescript; // to install
-tsc; // how all the commands.
-tsc. / app.ts; // in current directory transpile ts file into js.
-tsc - w. / apps.ts; // transpile in the watch mode.
+npm install - g typescript; // to install
+tsc; // see all the commands.
+tsc app.ts; // in current directory transpile ts file into js.
+tsc - w apps.ts; // transpile in the watch mode.
